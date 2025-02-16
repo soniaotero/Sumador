f8846e189da8e6991a957747feb3e31316f8b848
Sonia Otero <soniaoterofernandez@gmail.com>
2025-02-16 23:35:01 +0100

    Engadida clase Restador e modificada NewMain

--- src/main/java/sumador/sumador/Restador.java
+++ src/main/java/sumador/sumador/Restador.java
@@ -8,9 +8,10 @@ package sumador.sumador;
  *
  * @author sonia
  */
-public class NewMain {
-     public static void main(String[] args) {
-        System.out.println(Restador.restar(15f, 4f));
+public class Restador {
+    public static float restar(float a, float b) {
+        return a - b;
     }
+
     
 }
