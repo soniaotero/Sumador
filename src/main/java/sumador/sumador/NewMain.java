97482b88480560dcda908e9e06691011d5ad9e33
Sonia Otero <soniaoterofernandez@gmail.com>
2025-02-16 20:08:43 +0100

    Primeiro commit do proxecto

---
+++ src/main/java/sumador/sumador/NewMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumador.sumador;
+
+/**
+ *
+ * @author sonia
+ */
+public class NewMain {
+     public static void main(String[] args) {
+        System.out.println(Sumador.sumar(15f, 4f));
+    }
+    
+}
---
+++ src/main/java/sumador/sumador/Sumador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sumador.sumador;
+
+/**
+ *
+ * @author sonia
+ */
+public class Sumador {
+
+     public static float sumar(float a, float b) {
+        return a + b;
+    }
+}
